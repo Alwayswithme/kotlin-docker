09d0f337bbf9318c733b9dbf43a61b2523d1e0af
Alwayswithme <phx13ye@gmail.com>
2017-03-13 13:32:37 +0800

    configure dev env for idea

--- src/main/kotlin/me/phx/KotlinDockerApplication.kt
+++ src/main/kotlin/me/phx/KotlinDockerApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class KotlinDockerApplication
+open class KotlinDockerApplication
 
 fun main(args: Array<String>) {
     SpringApplication.run(KotlinDockerApplication::class.java, *args)
